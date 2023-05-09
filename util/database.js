f50d3b3926f0bae14014ada10988346101afdb11
JainPriya1234 <priya.jain11302@gmail.com>
2023-05-09 21:59:43 +0530

    env updated

--- util/database.js
+++ util/database.js
@@ -2,7 +2,7 @@ const mongodb =require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 const mongoConnect = (callback) => {
-    MongoClient.connect('mongodb+srv://Priya:abcd@cluster0.ybb47ei.mongodb.net/test')
+    MongoClient.connect(process.env.MDB)
     .then(client => {
         console.log('connected');
         callback(client);
